82e8141a2736de3c283143f32abba4dc74a1a1db
Steve Walker <captnwalker@gmail.com>
2018-07-25 23:52:52 -0400

    minimal viable product

---
+++ src/app/components/AstronomyContainer.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import AstronomyCard from './AstronomyCard';
+import axios from 'axios';
+
+class AstronomyContainer extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            astronomy: []
+        }
+    }
+
+componentDidMount() {
+    const API_KEY = 'QKjaHDwn1XIavrUNEL4k9ITa8gPSORm3Npc5xVSK';
+    const END_POINT = 'https://api.nasa.gov/planetary/apod?api_key='
+
+    axios.get(END_POINT+API_KEY)
+        .then(response => {
+
+            this.setState({
+                astronomy: response.data
+            })
+
+            console.log(response)
+        })
+
+        .catch(error => {
+            console.log(error, 'failed to fetch data')
+        });
+    }
+
+    render() {
+        const { astronomy } = this.state;
+        return (
+            <AstronomyCard data={astronomy} />
+        )
+    }
+}
+
+export default AstronomyContainer;
